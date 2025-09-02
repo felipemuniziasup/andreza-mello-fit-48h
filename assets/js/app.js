d4cd8bf774ba0e46355ebc3afa0df7d0cf21fa96
felipemuniziasup@gmail.com <felipemuniziasup@gmail.com>
2025-09-01 21:40:26 -0300

    Deploy automático em 2025-09-01 21:40 [v1.0.3]

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
-// Scroll suave para âncoras internas
+// Rolagem suave para âncoras internas (#id)
 (function () {
   document.querySelectorAll('a[href^="#"]').forEach(a => {
-    a.addEventListener('click', e => {
+    a.addEventListener('click', (e) => {
       const id = a.getAttribute('href');
       if (!id || id === '#') return;
       const el = document.querySelector(id);
